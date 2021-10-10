da893764f0b34f9dcf330f029b3e8dc9393d8a4f
loglob <23386237+loglob@users.noreply.github.com>
2021-10-10 02:36:58 +0200

    Added Equality overloads to HandleWrapper

--- HandleWrapper.cs
+++ HandleWrapper.cs
@@ -23,7 +23,22 @@ namespace Dwarf
 		internal T wrapGetter<T>(getter<T> g)
 			=> g(Handle, out T v, out IntPtr err).handle(g.Method.Name, err, v);
 
+		internal T? wrapOptGetter<T>(getter<T> g) where T : struct
+			=> wrapGetter(g, out T v) ? v : null;
+
 		internal bool wrapGetter<T>(getter<T> g, out T val)
 			=> g(Handle, out val, out IntPtr err).handleOpt(g.Method.Name, err);
+
+		public override bool Equals(object obj)
+			=> obj is HandleWrapper hw && hw == this;
+
+		public override int GetHashCode()
+			=> Handle.GetHashCode();
+
+		public static bool operator ==(HandleWrapper l, HandleWrapper r)
+			=> l.Handle == r.Handle;
+
+		public static bool operator !=(HandleWrapper l, HandleWrapper r)
+			=> l.Handle != r.Handle;
 	}
 }
